4fda5280835c1959643ae103c5ed297f56017a37
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2020-04-16 16:11:00 +0200

    operatpr+ overloads

---
+++ source/HolisticWare.Core.Math.Matrix.netstandard10/Vector.Operators.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Core.Math.Matrix
+{
+    /// <see cref="https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/operator-overloading"/>
+    /// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/operator-overloading#overloadable-operators
+    public partial class Vector<T>
+    {
+        public static Vector<int> operator+(Vector<T> lhs, Vector<int> rhs)
+        {
+            return lhs.Add(rhs);
+        }
+
+        public static Vector<int> operator +=(Vector<int> rhs)
+        {
+            return this + rhs;
+        }
+
+        public Vector<int> Add(Vector<int> rhs)
+        {
+            if (this.Length != rhs.Length)
+            {
+                throw new System.InvalidOperationException("Vectors must be the same size!");
+            }
+
+            int length = this.Length;
+            int[] tmp_data = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                int? l = this.Data[i] as int?;
+                int r = rhs.Data[i];
+                tmp_data[i] = r + l.Value;
+            }
+
+            Vector<int> vector_result = new Vector<int>(length);
+            vector_result.Data = tmp_data;
+
+            return vector_result;
+        }
+    }
+
+}
--- source/HolisticWare.Core.Math.Matrix.netstandard10/Vector.cs
+++ source/HolisticWare.Core.Math.Matrix.netstandard10/Vector.cs
@@ -1,17 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Core.Math.Matrix
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// https://github.com/dotnet/runtime/blob/master/src/libraries/System.Numerics.Vectors/ref/System.Numerics.Vectors.cs
     public partial class Vector<T>
-    {
-        T[] data;
-        int vector_size = 0;
+        where T :
+            struct,
+            IComparable,
+            IComparable<T>,
+            IConvertible,       // netstandard1.3
+            IEquatable<T>,
+            IFormattable {
+        protected int length;
 
-        protected Vector()
-        {
+        public int Length {
+            get {
+                return this.data.Length;
+            }
+            protected set {
+                length = value;
+            }
+        }
+
+        /// <summary>
+        /// Data
+        /// </summary>
+        public T[] Data {
+            get {
+                return data;
+            }
+            set {
+                data = value;
+            }
+        }
+        protected T[] data;
+
+        /// <summary>
+        /// Indexer
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public T this[int i] {
+            get {
+                return data[i];
+            }
+            set {
+                data[i] = value;
+            }
+        }
+
+
+        protected Vector() {
             data_initializer = null;
-            data = new T[vector_size];
+            data = new T[length];
 
             return;
         }
@@ -21,12 +68,11 @@ namespace Core.Math.Matrix
         /// By default 1x1 - scalar
         /// </summary>
         /// <param name="size">Size of the vector</param>
-        public Vector(int size = 1) 
-        {
+        public Vector(int size = 1) {
             data_initializer = null;
 
-            vector_size = size;
-            data = new T[vector_size];
+            length = size;
+            data = new T[length];
 
             return;
         }
@@ -36,11 +82,9 @@ namespace Core.Math.Matrix
         /// </summary>
         /// <param name="value">Initial value</param>
         /// <param name="size">Size of the vector</param>
-        public Vector(T value, int size = 1) : this(size)
-        {
-            for(int i = 1; i <= size; i++)
-            {
-                data[i - 1] = value;
+        public Vector(T value, int size = 1) : this(size) {
+            for (int i = 1; i <= size; i++) {
+                Data[i - 1] = value;
             }
 
             return;
@@ -50,42 +94,18 @@ namespace Core.Math.Matrix
         /// Vector output (C# frinedly)
         /// </summary>
         /// <returns>Vector as C# friendly string </returns>
-        public override string ToString()
-        {
+        public override string ToString() {
             string name = this.GetType().Name;
 
             System.Text.StringBuilder sb = new System.Text.StringBuilder();
             sb.Append("vector[] = { ");
-            for (int i = 0; i < data.Length; i++)
+            for (int i = 0; i < this.Length; i++)
             {
-                sb.Append($"{data[i]} ,");
+                sb.Append($"{this.Data[i]} ,");
             }
             sb.Append("};");
 
             return sb.ToString();
         }
-
-        public static Vector<int> Add(Vector<int> lhs, Vector<int> rhs)
-        {
-            if (lhs.data.Length != rhs.data.Length)
-            {
-                throw new System.InvalidOperationException("Vectors must be the same size!");
-            }
-
-            int length = lhs.data.Length;
-            int[] tmp_data = new int[length];
-
-            for (int i = 0; i < length; i++)
-            {
-                tmp_data[i] = lhs.data[i] + rhs.data[i];
-            }
-
-            Vector<int> vector_result = new Vector<int>(length);
-            vector_result.data = tmp_data;
-
-            return vector_result;
-        }
-
     }
-
 }
