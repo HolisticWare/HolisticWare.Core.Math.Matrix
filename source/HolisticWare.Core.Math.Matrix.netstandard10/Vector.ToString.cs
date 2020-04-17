938d8792d4a54f1d262ef62587c6663054aaabeb
moljac <mcvjetko@holisticware.net>
2020-04-17 10:43:24 +0200

    Create Vector.ToString.cs

---
+++ source/HolisticWare.Core.Math.Matrix.netstandard10/Vector.ToString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Core.Math.Matrix
+{
+    public partial class Vector<T>
+        //:
+        //IFormattable
+    {
+    }
+}
